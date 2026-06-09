b17566fdb30a76a0f257454e04b28715e7c55211
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-09 14:43:17 +0900

    add: section4 練習問題の実施

--- src/lesson/defaultArgument.js
+++ src/lesson/defaultArgument.js
@@ -1,8 +1,8 @@
 // ここにコードを書きながら確認しましょう！
 
+console.log(discountRateCalc(4500));
 
-
-
+console.log(discountRateCalc(6000, 20));
 
 
 
@@ -13,6 +13,6 @@
 
 // 練習問題
 // 問題 1
-function discountRateCalc(price, rate) {
+function discountRateCalc(price, rate = 5) {
   return price * (1 - rate * 0.01);
 }
