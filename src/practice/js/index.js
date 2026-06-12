67e9144322c0fa1183aaedce8b619e624f5419ac
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-12 10:57:21 +0900

    add: モジュールのimport

--- src/practice/js/index.js
+++ src/practice/js/index.js
@@ -1,4 +1,6 @@
 // 必要な関数をモジュールからインポートする
+import { $axios } from "./axiosHelper.js";
+import { createErrorElement, createElements } from "./createElement.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   // ここに処理を書いていく
