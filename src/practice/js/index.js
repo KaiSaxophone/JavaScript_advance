14d220aa53a8a081d627a474c923fe8b61a2ebcc
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-15 10:39:14 +0900

    feat: Promise.allでポケモン図鑑順に表示するよう修正

--- src/practice/js/index.js
+++ src/practice/js/index.js
@@ -8,48 +8,63 @@ window.addEventListener("DOMContentLoaded", () => {
   const list = document.querySelector(".list");
 
   $axios("https://pokeapi.co/api/v2/pokemon/?limit=151")
-    .then((response) => {
+    .then(response => {
       const pokemonList = response.data.results;
-      console.log(pokemonList);
 
-      // ポケモンの一覧情報がconsoleに表示される
-      pokemonList.forEach(pokemonData => {
-        $axios(pokemonData.url)
-          .then((response) => {
+      const promiseList = pokemonList.map(pokemonData => {
+
+        let pokemonImg;
+        let japanesePokemonName;
+
+        return $axios(pokemonData.url)
+          .then(response => {
             // 画像を取得
-            const pokemonImg = response.data.sprites.other["official-artwork"].front_default;
+            pokemonImg = response.data.sprites.other["official-artwork"].front_default;
 
-            $axios(response.data.species.url).then((response) => {
-              // 日本語名を取得
-              const japanesePokemonName = response.data.names[0].name;
-
-              // HTML要素を生成
-              const pokemonHTML = 
-              `
-                <li class="list-item">
-                  <div class="character">
-                    <img src="${pokemonImg}" width="475" height="475" alt="" class="character__img">
-                  </div>
-                  <p class="character__name">${japanesePokemonName}</p>
-                </li>
-              `
-              const fragment = createElements(pokemonHTML);
-              list.appendChild(fragment);
-            })
-            .catch((message) => {
-              createErrorElement(message);
-              console.log(message);
-            });
+            // thenメソッドにpromiseを返す
+            return $axios(response.data.species.url);
           })
-          .catch((message) => {
+
+          .then(response => {
+            // 日本語名を取得
+            japanesePokemonName = response.data.names[0].name;
+
+            return { pokemonImg, japanesePokemonName };
+          })
+
+          .catch(message => {
             createErrorElement(message);
             console.log(message);
           });
       });
+
+      Promise.all(promiseList)
+        .then(promiseList => {
+          for (const pokemon of promiseList) {
+
+            // HTML要素を生成
+            const pokemonHTML =
+            `
+              <li class="list-item">
+                <div class="character">
+                  <img src="${pokemon.pokemonImg}" width="475" height="475" alt="" class="character__img">
+                </div>
+                <p class="character__name">${pokemon.japanesePokemonName}</p>
+              </li>
+            `
+            const fragment = createElements(pokemonHTML);
+            list.appendChild(fragment);
+          }
+        })
+
+        .catch(message => {
+          createErrorElement(message);
+          console.log(message);
+        });
     })
 
     .catch((message) => {
       createErrorElement(message);
       console.log(message);
     });
-});
+})
